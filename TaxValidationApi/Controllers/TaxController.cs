3fabb20de9a5edfaf1053d24c4032f92e25edc80
Le Tuan Anh <tuananh-le@outlook.com>
2019-07-11 02:19:36 +1000

    Add project files.

---
+++ TaxValidationApi/Controllers/TaxController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaxValidationApi.Services;
+
+namespace TaxValidationApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaxController : ControllerBase
+    {
+        private readonly ITaxService _taxService;
+
+        public TaxController(ITaxService taxService)
+        {
+            _taxService = taxService;
+        }
+
+        // GET api/tax/validity/5
+        [HttpGet("Validity/{tfn}")]
+        public ActionResult<bool> GetTfnValidity(int tfn)
+        {
+            return _taxService.GetTfnValidity(tfn);
+        }
+    }
+}
