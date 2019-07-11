9f47b151de3f679b5ace060e487870586f1a23ed
Tuan Anh Le <tuan.anhle@beteasy.com.au>
2019-07-11 14:26:23 +1000

    Removed unused using statements

--- TaxValidationApi/Services/TaxService/ITaxService.cs
+++ TaxValidationApi/Services/TaxService/ITaxService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace TaxValidationApi.Services.TaxService
+﻿namespace TaxValidationApi.Services.TaxService
 {
     public interface ITaxService
     {
